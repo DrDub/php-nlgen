b868c63760f0455c3f25dd3e563031d8ffdb122f
Pablo Duboue <pablo.duboue@gmail.com>
2016-09-08 05:33:24 -0400

    STE update

--- examples/ste/ste.php
+++ examples/ste/ste.php
@@ -24,7 +24,12 @@
  */
 
 use nlgen\Generator;
-require '../../php-nlgen/generator.php';
+
+global $argv,$argc;
+
+if($argc>2){
+    require '../../php-nlgen/generator.php';
+}
 
 // done at the INLG2016 hackathon
 // takes as input as a stripped version of the Stanford Parser Universal dependencies
@@ -76,55 +81,43 @@ class SimpleTechnicalEnglish extends Generator {
 
   protected function root($entry){
       $result = $entry['head'];
-      if(isset($entry['cop'])){
-          foreach($entry['cop'] as $cop){
-              $result=$this->cop($cop).' '.$result;
-          }
-      }
-      if(isset($entry['dobj'])){
-          foreach($entry['dobj'] as $dobj){
-              $result=$result.' '.$this->dobj($dobj);
-          }
-      }
-      if(isset($entry['mark'])){
-          foreach($entry['mark'] as $mark){
-              $result=$this->mark($mark).' '.$result;
-          }
-      }
-      if(isset($entry['dep'])){
-          foreach($entry['dep'] as $dep){
+      if(isset($entry['compound'])){ foreach($entry['compound'] as $compound){ $result=$this->compound($compound).' '.$result; } }
+      if(isset($entry['det'])){ foreach($entry['det'] as $det){ $result=$this->det($det).' '.$result; } }
+      if(isset($entry['advmod'])){ foreach($entry['advmod'] as $advmod){ $result=$this->advmod($advmod).' '.$result; } }
+      if(isset($entry['neg'])){ foreach($entry['neg'] as $neg){ $result=$this->advmod($neg).' '.$result; } }
+      if(isset($entry['aux'])){ foreach($entry['aux'] as $aux){ $result=$this->aux($aux).' '.$result; } }
+      if(isset($entry['auxpass'])){ foreach($entry['auxpass'] as $aux){ $result=$result.' '.$this->aux($aux); } }
+      if(isset($entry['cop'])){ foreach($entry['cop'] as $cop){ $result=$this->cop($cop).' '.$result; } }
+      if(isset($entry['dobj'])){ foreach($entry['dobj'] as $dobj){ $result=$result.' '.$this->dobj($dobj); } }
+      if(isset($entry['mark'])){ foreach($entry['mark'] as $mark){ $result=$this->mark($mark).' '.$result; } }
+      if(isset($entry['compound:prt'])){ foreach($entry['compound:prt'] as $compound){ $result=$result.' '.$this->compound($compound); } }
+      if(isset($entry['dep'])){ foreach($entry['dep'] as $dep){
               $generated = $this->dep($dep);
               if(count(split(' ',$generated)) > 1){
                   $result = $result.' '.$generated;
               }else{
                   $result=$generated.' '.$result;
               }
-          }
-      }
-      if(isset($entry['nmod'])){
-          foreach($entry['nmod'] as $nmod){
-              $result=$result.' '.$this->nmod($nmod);
-          }
-      }
-      if(isset($entry['xcomp'])){
-          foreach($entry['xcomp'] as $xcomp){
-              $result=$result.' '.$this->root($xcomp);
-          }
-      }
-      if(isset($entry['nsubj'])){
-          foreach($entry['nsubj'] as $nsubj){
-              $result=$this->dobj($nsubj).' '.$result;
-          }
-      }
-      if(isset($entry['ccomp'])){
-          foreach($entry['ccomp'] as $ccomp){
-              $result=$result.' '.$this->root($ccomp);
-          }
-      }
+          } }
+      if(isset($entry['nmod'])){ foreach($entry['nmod'] as $nmod){ $result=$result.' '.$this->nmod($nmod); } }
+      if(isset($entry['nmod:of'])){ foreach($entry['nmod:of'] as $nmod){ $result=$result.' '.$this->nmod($nmod); } }
+      if(isset($entry['nmod:with'])){ foreach($entry['nmod:with'] as $nmod){ $result=$result.' '.$this->nmod($nmod); } }
+      if(isset($entry['xcomp'])){ foreach($entry['xcomp'] as $xcomp){ $result=$result.' '.$this->root($xcomp); } }
+      if(isset($entry['nsubj'])){ foreach($entry['nsubj'] as $nsubj){ $result=$this->dobj($nsubj).' '.$result; } }
+      if(isset($entry['csubj'])){ foreach($entry['csubj'] as $csubj){ $result=$this->root($csubj).' '.$result; } }
+      if(isset($entry['nsubjpass'])){ foreach($entry['nsubjpass'] as $nsubj){ $result=$result.' '.$this->dobj($nsubj); } }
+      if(isset($entry['iobj'])){ foreach($entry['iobj'] as $iobj){ $result=$result.' '.$this->dobj($iobj); } }
+      if(isset($entry['ccomp'])){ foreach($entry['ccomp'] as $ccomp){ $result=$result.' '.$this->root($ccomp); } }
+      if(isset($entry['cc'])){ foreach($entry['cc'] as $cc){ $result=$result.' '.$this->cc($cc); } }
+      if(isset($entry['conj'])){ foreach($entry['conj'] as $conj){ $result=$result.' '.$this->root($conj); } }
+      if(isset($entry['advcl'])){ foreach($entry['advcl'] as $advcl){ $result=$result.' '.$this->root($advcl); } }
       foreach($entry as $key => $value){
           if($key != 'head' && $key != 'dep' && $key != 'dobj' && $key != 'nmod' && $key != 'mark'
-          && $key != 'xcomp' && $key != 'nsubj' && $key != 'ccomp' && $key != 'cop'){
-              $result.=' [MM-root:'.$key.']';
+          && $key != 'xcomp' && $key != 'nsubj' && $key != 'ccomp' && $key != 'cop' && $key != 'det'
+          && $key != 'compound:prt' && $key != 'nmod:with'  && $key != 'nmod:of'  && $key != 'nsubjpass'
+          && $key != 'aux' && $key != 'auxpass' && $key != 'advmod' && $key != 'cc' && $key != 'conj'
+          && $key != 'neg' && $key != 'iobj' && $key != 'csubj' && $key != 'advcl' && $key != 'compound'){
+              $result=$result.' [MM-root:'.$key.']';
           }
       }
       return $result;
@@ -132,33 +125,20 @@ class SimpleTechnicalEnglish extends Generator {
 
   protected function dobj($entry){
       $result = $entry['head'];
-      if(isset($entry['compound'])){
-          foreach($entry['compound'] as $compound){
-              $result=$this->compound($compound).' '.$result;
-          }
-      }
-      if(isset($entry['amod'])){
-          foreach($entry['amod'] as $amod){
-              $result=$this->amod($amod).' '.$result;
-          }
-      }      
-      if(isset($entry['det'])){
-          foreach($entry['det'] as $det){
-              $result=$this->det($det)." ".$result;
-          }
-      }
-      if(isset($entry['acl'])){
-          foreach($entry['acl'] as $acl){
-              $result=$result.' '.$this->root($acl);
-          }
-      }
-      if(isset($entry['nmod'])){
-          foreach($entry['nmod'] as $nmod){
-              $result=$result.' '.$this->nmod($nmod);
-          }
-      }
+      if(isset($entry['compound'])){ foreach($entry['compound'] as $compound){ $result=$this->compound($compound).' '.$result; } }
+      if(isset($entry['amod'])){ foreach($entry['amod'] as $amod){ $result=$this->amod($amod).' '.$result; } }      
+      if(isset($entry['nummod'])){ foreach($entry['nummod'] as $nummod){ $result=$this->nummod($nummod).' '.$result; } }
+      if(isset($entry['det'])){ foreach($entry['det'] as $det){ $result=$this->det($det)." ".$result; } }
+      if(isset($entry['neg'])){ foreach($entry['neg'] as $neg){ $result=$this->advmod($neg).' '.$result; } }
+      if(isset($entry['acl'])){ foreach($entry['acl'] as $acl){ $result=$result.' '.$this->root($acl); } }
+      if(isset($entry['acl:relcl'])){ foreach($entry['acl:relcl'] as $acl){ $result=$result.' '.$this->root($acl); } }
+      if(isset($entry['appos'])){ foreach($entry['appos'] as $appos){ $result=$result.' '.$this->appos($appos); } }
+      if(isset($entry['nmod'])){ foreach($entry['nmod'] as $nmod){ $result=$result.' '.$this->nmod($nmod); } }
+      if(isset($entry['nmod:of'])){ foreach($entry['nmod:of'] as $nmod){ $result=$result.' '.$this->nmod($nmod); } }
+      if(isset($entry['cc'])){ foreach($entry['cc'] as $cc){ $result=$result.' '.$this->cc($cc); } }
       foreach($entry as $key => $value){
-          if($key != 'head' && $key != 'det' && $key != 'acl' && $key != 'compound' && $key != 'amod' && $key != 'nmod'){
+          if($key != 'head' && $key != 'det' && $key != 'acl' && $key != 'compound' && $key != 'amod' && $key != 'nmod' && $key != 'appos'
+           && $key != 'nmod:of' && $key != 'acl:relcl' && $key != 'cc' && $key != 'nummod' && $key != 'neg'){
               $result.=' [MM-dobj:'.$key.']';
           }
       }
@@ -178,36 +158,12 @@ class SimpleTechnicalEnglish extends Generator {
   protected function nmod($entry){
       $result = $entry['head'];
       
-      if(isset($entry['compound'])){
-          foreach($entry['compound'] as $compound){
-              $result=$this->compound($compound).' '.$result;
-          }
-      }
-      if(isset($entry['amod'])){
-          foreach($entry['amod'] as $amod){
-              $result=$this->amod($amod)." ".$result;
-          }
-      }
-      if(isset($entry['nummod'])){
-          foreach($entry['nummod'] as $nummod){
-              $result=$this->nummod($nummod).' '.$result;
-          }
-      }
-      if(isset($entry['det'])){
-          foreach($entry['det'] as $det){
-              $result=$this->det($det)." ".$result;
-          }
-      }
-      if(isset($entry['case'])){
-          foreach($entry['case'] as $case){
-              $result=$this->_case($case).' '.$result;
-          }
-      }
-      if(isset($entry['nmod'])){
-          foreach($entry['nmod'] as $nmod){
-              $result=$result.' '.$this->nmod($nmod);
-          }
-      }
+      if(isset($entry['compound'])){ foreach($entry['compound'] as $compound){ $result=$this->compound($compound).' '.$result; } }
+      if(isset($entry['amod'])){ foreach($entry['amod'] as $amod){ $result=$this->amod($amod)." ".$result; } }
+      if(isset($entry['nummod'])){ foreach($entry['nummod'] as $nummod){ $result=$this->nummod($nummod).' '.$result; } }
+      if(isset($entry['det'])){ foreach($entry['det'] as $det){ $result=$this->det($det)." ".$result; } }
+      if(isset($entry['case'])){ foreach($entry['case'] as $case){ $result=$this->_case($case).' '.$result; } }
+      if(isset($entry['nmod'])){ foreach($entry['nmod'] as $nmod){ $result=$result.' '.$this->nmod($nmod); } }
       foreach($entry as $key => $value){
           if($key != 'head' && $key != 'case' && $key != 'det'&& $key != 'amod' && $key != 'compound' && $key != 'nmod' && $key != 'nummod'){
               $result.=' [MM-nmod:'.$key.']';
@@ -226,6 +182,16 @@ class SimpleTechnicalEnglish extends Generator {
       foreach($entry as $key => $value){
           if($key != 'head'){
               $result.=' [MM-compound:'.$key.']';
+          }
+      }
+      return $result;
+  }
+  
+  protected function advmod($entry){
+      $result = $entry['head'];
+      foreach($entry as $key => $value){
+          if($key != 'head'){
+              $result.=' [MM-advmod:'.$key.']';
           }
       }
       return $result;
@@ -251,6 +217,16 @@ class SimpleTechnicalEnglish extends Generator {
       return $result;
   }
 
+  protected function aux($entry){
+      $result = $entry['head'];
+      foreach($entry as $key => $value){
+          if($key != 'head'){
+              $result.=' [MM-aux:'.$key.']';
+          }
+      }
+      return $result;
+  }
+  
   protected function cop($entry){
       $result = $entry['head'];
       foreach($entry as $key => $value){
@@ -259,6 +235,16 @@ class SimpleTechnicalEnglish extends Generator {
           }
       }
       return $result;
+  }
+  
+  protected function appos($entry){
+      $result = $entry['head'];
+      foreach($entry as $key => $value){
+          if($key != 'head'){
+              $result.=' [MM-appos:'.$key.']';
+          }
+      }
+      return "(" . $result . ")";
   }
   
   protected function dep($entry){
@@ -291,10 +277,22 @@ class SimpleTechnicalEnglish extends Generator {
       return $result;
   }
   
-  protected function amod($entry){
+  protected function cc($entry){
       $result = $entry['head'];
       foreach($entry as $key => $value){
           if($key != 'head'){
+              $result.=' [MM-cc:'.$key.']';
+          }
+      }
+      return $result;
+  }
+  
+  protected function amod($entry){
+      $result = $entry['head'];
+      if(isset($entry['cc'])){ foreach($entry['cc'] as $cc){ $result=$this->cc($cc).' '.$result; } }
+      if(isset($entry['conj'])){ foreach($entry['conj'] as $conj){ $result=$this->amod($conj).' '.$result; } }
+      foreach($entry as $key => $value){
+          if($key != 'head' && $key != 'cc' && $key != 'conj'){
               $result.=' [MM-amod:'.$key.']';
           }
       }
@@ -303,8 +301,9 @@ class SimpleTechnicalEnglish extends Generator {
   
   protected function nummod($entry){
       $result = $entry['head'];
+      if(isset($entry['compound'])){ foreach($entry['compound'] as $compound){ $result=$this->compound($compound).' '.$result; } }
       foreach($entry as $key => $value){
-          if($key != 'head'){
+          if($key != 'head' && $key != 'compound'){
               $result.=' [MM-nummod:'.$key.']';
           }
       }
@@ -312,7 +311,6 @@ class SimpleTechnicalEnglish extends Generator {
   }
 }
 
-global $argv,$argc;
 
 $examples = array(
 <<<HERE
@@ -451,10 +449,11 @@ function parse_deps($deps) {
 
 $gen = SimpleTechnicalEnglish::NewSealed();
 
-foreach($examples as $example){
-    print ucfirst($gen->generate(parse_deps($example))).".\n";
+if($argc>2){
+    foreach($examples as $example){
+        print ucfirst($gen->generate(parse_deps($example))).".\n";
+    }
 }
-
 //print_r($gen->semantics());
 
 
